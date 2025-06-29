caf51b5a881c70c1089626bf5f95705e12f98bf2
Felipe-Victorino <felipe.flvictorino@gmail.com>
2025-06-29 11:49:21 -0300

    Get_next_instruction atualizado

--- fetch.c
+++ fetch.c
@@ -3,5 +3,6 @@
 #include "reg.h"
 
 uint16_t get_next_instruction(uint16_t *memory){
-    memcpy(instruction, memory, 2);
+    uint16_t cur_memory = memory[2]; //substituir 2 pelo program counter
+    memcpy(instruction, cur_memory, sizeof(cur_memory));
 }
