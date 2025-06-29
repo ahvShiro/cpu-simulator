ae44316f703496386ade30aa0359a9b9121e61bb
Felipe-Victorino <felipe.flvictorino@gmail.com>
2025-06-29 12:13:18 -0300

    fetch.c consertado, ainda tem que descobrir como implementar o program counter

--- fetch.c
+++ fetch.c
@@ -2,7 +2,7 @@
 #include "fetch.h"
 #include "reg.h"
 
-uint16_t get_next_instruction(uint16_t *memory){
-    uint16_t cur_memory = memory[2]; //substituir 2 pelo program counter
-    memcpy(instruction, cur_memory, sizeof(cur_memory));
+uint16_t * get_next_instruction(uint16_t *memory){
+    memcpy(instruction, memory, sizeof(instruction));
+    return instruction;
 }
--- lib.h
+++ lib.h
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <assert.h>
+#include <string.h>
 
 #define LENGTH(a) (sizeof(a) / sizeof((a)[0]))
 
