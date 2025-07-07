702b5977ce8977cf1e703125675aeb268be148c2
Felipe-Victorino <felipe.flvictorino@gmail.com>
2025-07-07 13:31:29 -0300

    Create i instruction first draft

--- decode.c
+++ decode.c
@@ -6,9 +6,17 @@
 R_format create_r_instruction (uint16_t instruction)
 {
     R_format ins;
-    ins.opcode = extract_bits(instruction, 9, 14);
-    ins.dest = extract_bits(instruction, 6, 8);
-    ins.op1 = extract_bits(instruction, 3, 5);
-    ins.op2 = extract_bits(instruction, 0, 2);
+    ins.opcode = extract_bits(instruction, 9, 16);
+    ins.dest = extract_bits(instruction, 6, 16);
+    ins.op1 = extract_bits(instruction, 3, 16);
+    ins.op2 = extract_bits(instruction, 0, 16);
+    return ins;
+}
+
+I_format create_i_intruction (uint16_t instruction) {
+    I_format ins;
+    ins.opcode = extract_bits(instruction, 0, 16);
+    ins.reg = extract_bits(instruction, 0, 16);
+    ins.immd = extract_bits(instruction, 0, 16);
     return ins;
 }
