520213016451f817991be8fafe9e496dcb6e5bb7
Felipe-Victorino <felipe.flvictorino@gmail.com>
2025-07-14 11:14:56 -0300

    Added execute header

---
+++ execute.h
@@ -0,0 +1,18 @@
+//
+// Created by fflv on 14/07/25.
+//
+
+#ifndef EXECUTE_H
+#define EXECUTE_H
+
+#include "lib.h"
+
+void main_loop();
+
+void execute_i(I_format ins);
+
+void execute_r(R_format ins);
+
+
+
+#endif //EXECUTE_H
