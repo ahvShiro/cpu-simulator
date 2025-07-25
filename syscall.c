b02f08f0a259f5005da114c0058fc190e5f7ca87
Felipe-Victorino <felipe.flvictorino@gmail.com>
2025-07-25 14:28:42 -0300

    Added print integer and println syscalls

--- syscall.c
+++ syscall.c
@@ -1,6 +1,5 @@
 //
 // Created by fflv on 25/07/25.
-//
 
 #include "syscall.h"
 
@@ -21,6 +20,25 @@ void syscall_routine(RegFile *rf, uint16_t *memory)
 
         }
         break;
+    case 2:
+        //print integer
+        uint16_t c = rf->r1;
+        while (memory[c] != 0)
+        {
+            printf("%d", memory[c]);
+            c++;
+
+        }
+    case 3:
+        // print string with newline
+        uint16_t c = rf->r1;
+        while (memory[c] != 0)
+        {
+            printf("%c", memory[c]);
+            c++;
+
+        }
+        printf("\n");
     default:
         printf("System call doesn't exist");
         free(memory);
